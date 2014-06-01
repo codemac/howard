048405433ee95e13ebc64f791216cbbc3cb119af
Jeff Mickey <j@codemac.net>
2014-05-31 18:38:12 -0700

    GPLv3

--- howard.go
+++ howard.go
@@ -1,6 +1,22 @@
+// howard - a goddamned window manager
+// Copyright (C) 2014- Jeff Mickey
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/BurntSushi/xgb"
 	"github.com/BurntSushi/xgb/randr"
@@ -8,6 +24,12 @@ import (
 )
 
 func main() {
+	fmt.Println("howard - Copyright (C) 2014 Jeff Mickey")
+	fmt.Println("This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.")
+	fmt.Println("This is free software, and you are welcome to redistribute it")
+	fmt.Println("under certain conditions; type `show c' for details.")
+
+	flag.Parse()
 	X, err := xgb.NewConn()
 	if err != nil {
 		panic("No display connection can be made!")
